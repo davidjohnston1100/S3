62fd36f0f5975a335bd2662ca4c233434e881943
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-09 21:53:14 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,23 @@ resource "aws_s3_bucket_versioning" "this" {
 }
   
 resource "aws_s3_bucket_accelerate_configuration" "this" {
-  bucket = aws_s3_bucket.thiws.bucket
+  bucket = aws_s3_bucket.this.bucket
   status = "Enabled"
 }
+  
+resource "aws_kms_key" "this" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.this.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}  
 
