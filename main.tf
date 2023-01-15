3603a85d7e91c2e3a080162eff9b4cd86bcca1e4
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-15 16:30:25 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,9 @@ provider "aws" {
 module "S3" {
   source = "./modules/S3"
 }
+  
+  resource "aws_instance" "example" {
+  ami           = "ami-0a606d8395a538502"
+  instance_type = "t2.micro"
+  iam_instance_profile = module.S3.iam_role_arn
+}
