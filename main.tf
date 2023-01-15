7e2cd884f3ee012623ce9b5888b8c4968c713f5d
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-15 16:51:28 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,5 +19,6 @@ module "S3" {
   ami           = "ami-0a606d8395a538502"
   instance_type = "t2.micro"
   #iam_instance_profile = module.S3.iam_role_arn
-  iam_instance_profile =  module.S3.iam_role_policy_attachment_arn
+  #iam_instance_profile =  module.S3.iam_role_policy_attachment_arn
+  iam_instance_profile = module.S3.iam_role_id
 }
