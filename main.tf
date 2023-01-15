b797db60e05ae64cd4c772a604c76247d8479d40
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-15 17:28:38 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,8 +18,5 @@ module "S3" {
   resource "aws_instance" "example" {
   ami           = "ami-0a606d8395a538502"
   instance_type = "t2.micro"
-  #iam_instance_profile = module.S3.iam_role_arn
-  #iam_instance_profile =  module.S3.iam_role_policy_attachment_arn
-  #iam_instance_profile = module.S3.iam_policy_arn
   iam_instance_profile =  module.S3.aws_iam_instance_profile_id
 }
