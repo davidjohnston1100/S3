d55e6b7cf33ee3bb73a8851e46c9ce67e7dbd07c
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-15 15:08:51 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,3 +37,53 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
     }
   }
 }  
+  
+#create read write s3 access for ec2 instance  
+resource "aws_iam_policy" "s3_access" {
+  name = "s3-access"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "arn:aws:s3:::${aws_s3_bucket.this.arn}",
+        "arn:aws:s3:::${aws_s3_bucket.this.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+  
+  
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access" {
+  role = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_access.arn
+}
