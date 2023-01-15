caada1a5ea0b88310fd595721c7fd59078ec02e0
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-15 16:28:14 -0600

    Update outputs.tf

--- modules/S3/outputs.tf
+++ modules/S3/outputs.tf
@@ -2,3 +2,8 @@ output "s3_bucket_arn" {
   description = "S3 bucket arn"
   value       = aws_s3_bucket.this.arn
 }
+
+output "iam_role_arn" {
+  description = "S3 bucket arn"
+  value       = aws_iam_role.this.arn
+}
