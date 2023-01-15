fd7d818a53e35e1aaa42e5ade6db60103dd0eca8
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-15 17:29:16 -0600

    Update outputs.tf

--- modules/S3/outputs.tf
+++ modules/S3/outputs.tf
@@ -3,26 +3,6 @@ output "s3_bucket_arn" {
   value       = aws_s3_bucket.this.arn
 }
 
-output "iam_role_arn" {
-  description = "S3 bucket arn"
-  value       = aws_iam_role.ec2_role.arn
-}
-
-output "iam_role_id" {
-  description = ""
-  value       = aws_iam_role.ec2_role.id
-}
-
-output "iam_policy_arn" {
-  description = ""
-  value       = aws_iam_policy.s3_access.arn
-}
-
-output "aws_iam_instance_profile_arn" {
-  description = ""
-  value       = aws_iam_instance_profile.ec2_s3_read_write_access.arn
-}
-
 output "aws_iam_instance_profile_id" {
   description = ""
   value       = aws_iam_instance_profile.ec2_s3_read_write_access.id
