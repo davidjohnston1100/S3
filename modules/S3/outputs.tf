73970625d185781fb844b1daf98ad261dd5ed983
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-15 16:40:38 -0600

    Update outputs.tf

--- modules/S3/outputs.tf
+++ modules/S3/outputs.tf
@@ -7,3 +7,8 @@ output "iam_role_arn" {
   description = "S3 bucket arn"
   value       = aws_iam_role.ec2_role.arn
 }
+
+output "iam_role_policy_attachment_arn" {
+  description = ""
+  value       = aws_iam_role_policy_attachment.s3_access.arn
+}
