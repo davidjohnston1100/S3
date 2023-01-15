b5ccb1c4f900d2c682ce805eaebb4a7df0b9e591
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-15 15:55:39 -0600

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -1,1 +1,23 @@
+variable "bucket_name" {
+  description = "Name of bucket"
+  type        = string
+  default     = "1-15-2023-s3-testbucket9798"
+}
 
+variable "object_lock" {
+  description = ""
+  type        = string
+  default     = "Enabled"
+}
+
+variable "versioning_configuration" {
+  description = ""
+  type        = string
+  default     = "Enabled"
+}
+
+variable "bucket_accelerate" {
+  description = ""
+  type        = string
+  default     = "Enabled"
+}
