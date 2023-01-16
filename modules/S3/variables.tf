90891dd300f391a6c83647711b9e24b9c564357e
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-15 20:39:52 -0600

    Update variables.tf

--- modules/S3/variables.tf
+++ modules/S3/variables.tf
@@ -36,12 +36,12 @@ variable "transit_gateway_attachment_id_2" {
 
 variable "create_attachement_accepter_1" {
   description = ""
-  type        = number
-  default = 1
+  type        = bool
+  default = false
 }
 
 variable "create_attachement_accepter_2" {
   description = ""
-  type        = number
-  default = 1
+  type        = bool
+  default = false
 }
