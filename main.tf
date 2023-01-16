d9da83f1837dbdfc2753456ef2044b17d3ee0514
davidjohnston1100 <82924186+davidjohnston1100@users.noreply.github.com>
2023-01-15 21:12:53 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ module "S3" {
   source = "./modules/S3"
   
   transit_gateway_attachment_id_1 = "tgw-attach-0299516c7b365a51b"
+  transit_gateway_attachment_id_2 = "tgw-attach-001b8fba49a7e9549"
 }
   
   resource "aws_instance" "example" {
@@ -22,8 +23,3 @@ module "S3" {
   instance_type = "t2.micro"
   iam_instance_profile =  module.S3.aws_iam_instance_profile_id
 }
-    
-#create_attachement_accepter_1  = var.create_attachement_accepter_1
-#create_attachement_accepter_2 = 0
-
-#transit_gateway_attachment_id_2 = "tgw-attach-001b8fba49a7e9549"
